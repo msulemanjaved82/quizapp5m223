697c83bb8f039d05511260c9d2975a128d15115e
hammadYaseen666 <f21binft1m01073>
2024-01-23 20:34:18 +0500

    History screen designing and attempt section commit

--- quiz_app/lib/screens/quizAttempt.dart
+++ quiz_app/lib/screens/quizAttempt.dart
@@ -16,7 +16,7 @@ Future<List<QuizAttempt>> fetchQuizAttempts() async {
   List<QuizAttempt> quizAttempts = [
     QuizAttempt(attemptNumber: 1, correctAnswers: 8, totalQuestions: 10, status: 'Pass'),
     QuizAttempt(attemptNumber: 2, correctAnswers: 3, totalQuestions: 10, status: 'Fail'),
-    QuizAttempt(attemptNumber: 3, correctAnswers: 6, totalQuestions: 10, status: 'Passy'),
+    QuizAttempt(attemptNumber: 3, correctAnswers: 6, totalQuestions: 10, status: 'Pass'),
     // QuizAttempt(attemptNumber: 3, correctAnswers: 10, totalQuestions: 10, status: 'Pass'),
 
     // Add more quiz attempts as needed
